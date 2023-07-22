9c3e5beca595b24177ba2cd686b83961f605352c
Ramone Henry <ramoneh94@gmail.com>
2023-07-22 13:56:10 -0400

    test math functions

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,14 @@ fn two_minus(num1: i32, num2: i32) -> i32 {
     num1 - num2 
 }
 
+fn two_multiply(num1: i32, num2: i32) -> i32 {
+    num1 - num2
+}
+
+fn factoral(num1: i32) -> i32 {
+    num1 * num1
+}
+
 fn promote_love(name: &str) -> &str{
    println!("{0} love's everyone", name);
    return "declaration";
